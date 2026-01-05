5bf6afad0424db4c83472b7758c72cde47eac1d5
sandeepmukku <sandeep12akp@gmail.com>
2026-01-05 23:29:23 +0530

    Setup MongoDB connection

--- server/server.js
+++ server/server.js
@@ -4,16 +4,27 @@ const cors = require("cors");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+//MongoDB Connection
+mongoose
+  .connect(MONGO_DB_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error);
+  });
+
 app.get("/", (req, res) => {
-    res.send("API running successfully!");
+  res.send("API running successfully!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running successfully on port ${PORT}`);
+  console.log(`Server running successfully on port ${PORT}`);
 });
